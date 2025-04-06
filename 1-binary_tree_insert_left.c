d3fa7108d54b4a857e8be7c3842ecc4abff8cb81
Riyadh Alhamad <riyadh.it.dev@gmail.com>
2025-04-06 10:07:01 +0000

    edit file 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,7 +15,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 
-	my_node = malloc(sizeof(binary_tree_t);
+	my_node = malloc(sizeof(binary_tree_t));
 	
 	if (my_node == NULL)
 		return (NULL);
