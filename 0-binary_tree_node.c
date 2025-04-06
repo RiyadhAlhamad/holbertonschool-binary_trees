18b936fdb8bb53da70ce157a966bf5bbd68daf8d
Riyadh Alhamad <riyadh.it.dev@gmail.com>
2025-04-06 09:05:52 +0000

    add file 0

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+
+	binary_tree_t *my_node = malloc (sizeof (binary_tree_t));
+
+	if (my_node == NULL)
+		return NULL;
+
+	my_node->n = value;
+	my_node->parent = parent;
+	my_node->left = NULL;
+	my_node->right = NULL;
+
+	return (my_node);
