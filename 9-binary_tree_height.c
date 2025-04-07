7d66145fabc292eea022d1b7f900c08164bea10e
RiyadhAlhamad <112124978+RiyadhAlhamad@users.noreply.github.com>
2025-04-07 07:46:22 +0300

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,6 +9,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t l_height, r_height;
 	size_t max_height;
+	
 	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
 
